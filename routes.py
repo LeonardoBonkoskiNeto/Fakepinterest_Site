b7d60003b55a73580e420a91b945c9f603a848bb
unknown <leonardobonkoskineto@gmail.com>
2026-07-09 14:00:12 -0300

    eu adicionei a aba de perfil

--- routes.py
+++ routes.py
@@ -4,5 +4,9 @@ from main import app
 def homepage():
     return "Pinterest meu primeiro site no ar!!!!"
 
+@app.route("/perfil")
+def perfil():
+    return "perfil do usuario"
+
 if __name__=="__main__":
     app.run(debug=True)
