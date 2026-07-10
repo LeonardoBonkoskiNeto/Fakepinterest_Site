0481435cfed1f5d0e5c9f6551fe1a8d1506a757d
unknown <leonardobonkoskineto@gmail.com>
2026-07-10 14:54:20 -0300

    criei o perfil de usuario

--- routes.py
+++ routes.py
@@ -7,7 +7,6 @@ def homepage():
 
 @app.route("/perfil")
 def perfil():
-    return "pagina de perfil"
-
+     return render_template("perfil.html")
 if __name__=="__main__":
     app.run(debug=True)
