c50aeeae20c3b991135cdc7d8d5e8bc66fb7a2c3
Leonardo Bonkoski Neto <leonardobonkoskineto@gmail.com>
2026-08-10 21:29:30 -0300

    adicionei um pequeno formulario na pagina

--- forms.py
+++ forms.py
@@ -1,23 +1,27 @@
 #criar formularios    
-from flask_wtf import Flaskform
+from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
-from Fakepinterest import Usuario
+from Fakepinterest.models import Usuario
 
-class FormLogin():
+
+class FormLogin(FlaskForm):
     email = StringField("E-mail", validators=[DataRequired(), Email()])
     senha = PasswordField("senha", validators=[DataRequired()])
-    botao_confirmaçao = SubmitField("fazer login")
+    botao_confirmacao = SubmitField("fazer login")
 
-class FormCriarConta():
-     email = StringField("E-mail", validators=[DataRequired(), Email()])
-     username = StringField("senha", validators=[DataRequired()])
-     senha = PasswordField("senha", validators=[DataRequired(), Length(6, 20)])
-     confirmacao_senha = PasswordField("senha", validators=[DataRequired(), EqualTo("senha")])
-     botao_confirmaçao = SubmitField("criar conta")
 
-     def validate_email(self, email):
-          usuario = Usuario.query.filyer_by(email=email.data).first()
+class FormCriarConta(FlaskForm):
+    email = StringField("E-mail", validators=[DataRequired(), Email()])
+    username = StringField("username", validators=[DataRequired()])
+    senha = PasswordField("senha", validators=[DataRequired(), Length(6, 20)])
+    confirmacao_senha = PasswordField(
+        "senha",
+        validators=[DataRequired(), EqualTo("senha")]  )
+    botao_confirmacao = SubmitField("criar conta")
 
-          if usuario:
-               return ValidationError("email ja cadastrado, faça log in para continuar")
+    def validate_email(self, email):
+     usuario = Usuario.query.filter_by(email=email.data).first()
+
+     if usuario:
+         raise ValidationError("email já cadastrado, faça login para continuar")
--- routes.py
+++ routes.py
@@ -1,13 +1,20 @@
 
 from flask import render_template, url_for
 from Fakepinterest import app
-from flask import login_required
+from flask_login import login_required
+from Fakepinterest.forms import FormLogin, FormCriarConta
 
 print("ID do app em routes:", id(app))
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def homepage():
-    return render_template("homepage.html")
+    formlogin = FormLogin()
+    return render_template("homepage.html", form=formlogin)
+
+@app.route("/criarconta", methods=["GET", "POST"])
+def criarconta():
+    formcriarconta = FormCriarConta()
+    return render_template("criarconta.html", form=formcriarconta)
 
 @app.route("/perfil/<browser>")
 @login_required
